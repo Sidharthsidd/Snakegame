2651ff4d5990a7ac77f13370c40ccaedefb6b0e1
Sidharthsidd <sidh22cs@cmrit.ac.in>
2024-07-14 21:37:51 +0530

    first commit1

--- snakegame/input.js
+++ snakegame/input.js
@@ -1,12 +1,21 @@
-let inputDirection ={x:0,y:0}
+let inputDirection = { x: 0, y: 0 };
 
-
-window.addEventListener('keydown',e=>{
-    switch(e.key){
-        case 'Arrowup':
-            inputDirection={x:0,y:-1}
-    }
-})
-export function getInputDirection(){
-    return inpurDirection
+// window.addEventListener('keydown',e=>{
+//     switch(e.key){
+//         case 'Arrowup':
+//             inputDirection={x:0,y:-1}
+//             break;
+//         case 'Arrowdown':
+//             inputDirection={x:0,y:1}
+//             break;
+//         case 'Arrowleft':
+//             inputDirection={x:-1,y:0}
+//             break;
+//         case 'ArrowRight':
+//             inputDirection={x:1,y:0}
+//             break;
+//     }
+// })
+export function getInputDirection() {
+  return inputDirection;
 }
